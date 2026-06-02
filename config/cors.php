6f8b560eaf74e25c0cb042b2cccf65920048a803
sts2025 <walakilanyanzi@gmail.com>
2026-06-02 10:28:02 +0300

    Update CORS configuration to allow multiple origins

--- config/cors.php
+++ config/cors.php
@@ -2,11 +2,21 @@
 
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://resto.agileaccounts.me'],
+
+    'allowed_origins' => [
+        'https://resto.agileaccounts.me',
+        'https://www.resto.agileaccounts.me',
+    ],
+
     'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
-    'supports_credentials' => false,
+
+    'supports_credentials' => true,
 ];
